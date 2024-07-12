280445ecc6c660db4778c518788f3ff9b644773d
Marisa <56473700+maris28@users.noreply.github.com>
2024-07-12 18:53:24 +0200

    fix iniciar php

--- modules/computo/install_apache.sh
+++ modules/computo/install_apache.sh
@@ -9,5 +9,8 @@ sudo yum install php  php-cli php-json  php-mbstring  -y
 sudo  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo  php composer-setup.php
 sudo  php -r "unlink('composer-setup.php');"
-sudo  php composer.phar require aws/aws-sdk-php
-sudo service httpd restart
+sudo COMPOSER_ALLOW_SUPERUSER=1 php composer.phar require aws/aws-sdk-php
+
+# Inicia Apache y habilita para que inicie en cada reinicio del sistema
+sudo systemctl httpd restart
+sudo systemctl enable httpd
